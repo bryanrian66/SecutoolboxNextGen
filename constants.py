d1f489428241db0fbf428b5e28997c5c7ce84b97
bryanrian66 <116961742+bryanrian66@users.noreply.github.com>
2025-06-17 11:01:18 +0700

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,13 @@
+# constants.py
+
+# Reset
+RESET = '\033[0m'
+
+# Styles
+BOLD = '\033[1m'
+
+# Colors
+LCYAN = '\033[96m'  # Light Cyan
+RED = '\033[91m'    # Red
+LPURPLE = '\033[94m' # Light Purple
+GREEN = '\033[92m'  # Green
